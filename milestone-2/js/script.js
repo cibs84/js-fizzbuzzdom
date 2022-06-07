fd6861ea71a6dd67c6eb09c07b370b4b0a6e0d83
cibs84 <d.tilesi@gmail.com>
2022-06-07 16:44:25 +0200

    milestone-2 - Structures Html, Css and Js

---
+++ milestone-2/js/script.js
@@ -0,0 +1,7 @@
+// Scrivi un programma che stampi i numeri da 1 a 100.
+// MILESTONE 1
+// Per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// Stampare in console.
+// MILESTONE 2
+// Per ogni numero, data una ul nel DOM, aggiungete un elemento html li con il numero o la stringa corretta (seguendo le regole della Milestone 1).
