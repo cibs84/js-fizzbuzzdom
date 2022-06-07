21345984905ee7e597eaeb8ea665a75cf0ae8f7f
cibs84 <d.tilesi@gmail.com>
2022-06-07 17:01:29 +0200

    milestone-2 - completed

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -5,3 +5,18 @@
 // Stampare in console.
 // MILESTONE 2
 // Per ogni numero, data una ul nel DOM, aggiungete un elemento html li con il numero o la stringa corretta (seguendo le regole della Milestone 1).
+
+const allBoxes = document.querySelector('.all-boxes');
+
+// CICLO FOR
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        allBoxes.innerHTML += `<li class="box">FizzBuzz</li>`;
+    } else if (i % 3 === 0) {
+        allBoxes.innerHTML += `<li class="box">Fizz</li>`;
+    } else if (i % 5 === 0) {
+        allBoxes.innerHTML += `<li class="box">Buzz</li>`;
+    } else {
+        allBoxes.innerHTML += `<li class="box">${i}</li>`;
+    }
+};
