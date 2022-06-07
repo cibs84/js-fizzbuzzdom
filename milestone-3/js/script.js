d4e4f03324e1c58bfbd1b21c70c7481d78c65955
cibs84 <d.tilesi@gmail.com>
2022-06-07 17:30:43 +0200

    milestone-3 - completed

--- milestone-3/js/script.js
+++ milestone-3/js/script.js
@@ -10,16 +10,27 @@
 // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5 (seguendo le regole della Milestone 1).
 // Per questa Milestone avete lo screenshot allegato al messaggio.
 
+// SELEZIONO L'ELEMENTO UL
 const allBoxes = document.querySelector('.all-boxes');
 
-// CICLO FOR
+// CICLO FOR PER CREAZIONE ELEMENTI LI
 for (let i = 1; i <= 100; i++) {
+    // SE i è MULTIPLO DI 3 E 5
     if (i % 3 === 0 && i % 5 === 0) {
-        allBoxes.innerHTML += `<li class="box">FizzBuzz</li>`;
+        let contentBox = 'FizzBuzz';
+        let classBox = 'multiple-3-5';
+        allBoxes.innerHTML += `<li class="box ${classBox}">${contentBox}</li>`;
+    // SE i è MULTIPLO DI 3
     } else if (i % 3 === 0) {
-        allBoxes.innerHTML += `<li class="box">Fizz</li>`;
+        let contentBox = 'Fizz';
+        let classBox = 'multiple-3';
+        allBoxes.innerHTML += `<li class="box ${classBox}">${contentBox}</li>`;
+    // SE i è MULTIPLO DI 5
     } else if (i % 5 === 0) {
-        allBoxes.innerHTML += `<li class="box">Buzz</li>`;
+        let contentBox = 'Buzz';
+        let classBox = 'multiple-5';
+        allBoxes.innerHTML += `<li class="box ${classBox}">${contentBox}</li>`;
+    // ALTRIMENTI
     } else {
         allBoxes.innerHTML += `<li class="box">${i}</li>`;
     }
