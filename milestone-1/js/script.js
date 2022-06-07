734acd64cb4281770589142a5edd128df3048948
cibs84 <d.tilesi@gmail.com>
2022-06-07 16:40:28 +0200

    milestone-1 - completed

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -6,5 +6,13 @@
 
 // CICLO FOR
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+};
